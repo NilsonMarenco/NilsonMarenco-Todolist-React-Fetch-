a25436c64ed660f508aeb2aaa9fb893131057137
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-03-11 13:57:45 +0000

    Terminado

---
+++ src/js/component/tarea.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+
+const URL = 'https://playground.4geeks.com/apis/fake/todos/user/nilsonmarenco';
+
+function Tarea() {
+  const [input, setInput] = useState('');
+  const [tareas, setTareas] = useState([]);
+
+  const manejarCambioInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const manejarAgregarTarea = async (e) => {
+    e.preventDefault();
+
+    const nuevasTareas = [...tareas, { id: Math.random(), label: input, done: false }];
+
+    await fetch(URL, {
+      method: 'PUT',
+      body: JSON.stringify(nuevasTareas),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    setTareas(nuevasTareas);
+
+    setInput('');
+  };
+
+  const manejarEliminarTarea = async (taskIndex) => {
+    const tareasFiltradas = tareas.filter((_, index) => index !== taskIndex);
+
+    await fetch(URL, {
+      method: 'PUT',
+      body: JSON.stringify(tareasFiltradas),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    setTareas(tareasFiltradas);
+    console.log('Updated API y updated local todolist');
+  };
+
+  const obtenerTareas = async () => {
+    const tareasRespuesta = await fetch(URL);
+    const tareasResueltas = await tareasRespuesta.json();
+    setTareas(tareasResueltas);
+  };
+
+  useEffect(() => {
+    obtenerTareas();
+  }, []);
+
+  return (
+    <>
+      <div className="app__container">
+        <h1>To-Do List</h1>
+        <div className="main-lista__container">
+          <form onSubmit={manejarAgregarTarea}>
+            <input
+              className="tarea-input"
+              type="text"
+              placeholder="Escribe tarea"
+              onChange={manejarCambioInput}
+              value={input}
+            />
+          </form>
+          <div className="lista__container">
+            <ul>
+              {'Tareas Agregadas'}
+              {tareas.length > 0 &&
+                tareas.map((tarea, index) => (
+                  <li key={index}>
+                    <p>{tarea.label}</p>
+                    <button onClick={() => manejarEliminarTarea(index)}>
+                      ✖️
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Tarea
